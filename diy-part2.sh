b6f75768ff18fb0f0c69d265cb18444dd7d0b09a
q1206577452 <1206577452@qq.com>
2020-10-12 20:41:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,4 @@
 
 # Modify default IPv
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/KERNEL_PATCHVER:=4.9/KERNEL_PATCHVER:=4.9/g' target/linux/ar71xx/Makefile
